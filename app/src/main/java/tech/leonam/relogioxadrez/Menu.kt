0417415b8fdd1917b03ac36668e0c7088d6f803e
leonam <leonam447@gmail.com>
2023-06-26 17:20:19 -0300

    Menu Principal Funcionando

--- app/src/main/java/tech/leonam/relogioxadrez/Menu.kt
+++ app/src/main/java/tech/leonam/relogioxadrez/Menu.kt
@@ -1,27 +1,34 @@
 package tech.leonam.relogioxadrez
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import tech.leonam.relogioxadrez.databinding.ActivityMenuBinding
 
 class Menu : AppCompatActivity() {
     private lateinit var binding: ActivityMenuBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window!!.statusBarColor = Color.BLACK
+        supportActionBar!!.hide()
         binding = ActivityMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
         clickInit()
     }
 
     private fun clickInit() {
-        binding.init.setOnClickListener{
-            val minutos = binding.min.text.toString().toInt() * 60
-            val segundos = binding.seg.text.toString().toInt() + minutos
-
-            val intent = Intent(this,MainActivity::class.java)
-            intent.putExtra("seg",segundos)
-            startActivity(intent)
+        binding.init.setOnClickListener {
+            if (binding.min.text.toString().isBlank()) {
+                binding.min.setText("0")
+            } else if (binding.seg.text.toString().isBlank()) {
+                binding.seg.setText("0")
+            }
+                val minutos = binding.min.text.toString().toInt() * 60
+                val segundos = binding.seg.text.toString().toInt() + minutos
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("seg", segundos)
+                startActivity(intent)
         }
     }
 }
